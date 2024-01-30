5c54596612dc0f351689ca37da5de642c7ee540c
LE BARS Victor <Victor@ar155065.univ-rennes1.fr>
2024-01-30 15:05:24 +0100

    add list of pathways & gene associated + barplot

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ if (!require(plotly)) install.packages("plotly")
 if (!require(gprofiler2)) install.packages("gprofiler2")
 if (!require(dipsaus)) install.packages("dipsaus")
 if (!require(dplyr)) install.packages("dplyr")
+#if (!require(shinycssloaders)) install.packages("shinycssloaders")
 
 library(shiny)
 library(DT)
@@ -45,7 +46,12 @@ ui <- fluidPage(
     
     mainPanel(
       DTOutput("table"),
-      uiOutput("plot")
+      hr(),
+      fluidRow(
+        column(6, uiOutput("plot")),
+        column(6, uiOutput("plotPathways"))
+      ),
+      DTOutput("gprofilerTable")
     )
   )
 )
@@ -64,13 +70,11 @@ server <- function(input, output, session) {
     thresholdLOG2FC <- input$thresholdSliderLOG2FC
     thresholdPADJ <- input$thresholdSliderPADJ
     # To filter dataframe with chosen thresholds in Log2FoldChange & padj
-    if(input$DEside == "both"){
+    if (input$DEside == "both") {
       filtered_data <- data()[abs(data()$log2FoldChange) >= thresholdLOG2FC & data()$padj <= thresholdPADJ, ]
-    }
-    else if(input$DEside == "up"){
+    } else if (input$DEside == "up") {
       filtered_data <- data()[data()$log2FoldChange >= thresholdLOG2FC & data()$padj <= thresholdPADJ, ]
-    }
-    else if(input$DEside == "down"){
+    } else if (input$DEside == "down") {
       filtered_data <- data()[data()$log2FoldChange <= 0-thresholdLOG2FC & data()$padj <= thresholdPADJ, ]
     }
     
@@ -78,14 +82,12 @@ server <- function(input, output, session) {
     filtered_data <- filtered_data[rowSums(is.na(filtered_data)) != ncol(filtered_data), ]
     
     # Get the full dataframe even with non-expressed genes (with NA in Log2FoldChange and padj) when thresholds are by default
-    if (thresholdLOG2FC == 0 & thresholdPADJ == 1){
+    if (thresholdLOG2FC == 0 & thresholdPADJ == 1) {
       filtered_data <- data()
     }
-    
     if (input$KeepCodingGenes == TRUE) {
       filtered_data <- filtered_data[filtered_data$gene_biotype == "protein_coding",]
     }
-    
     if (input$KeepKnownGenes == TRUE) {
       filtered_data <- filtered_data[startsWith(filtered_data$geneID,"ENSG"),]
     }
@@ -95,7 +97,7 @@ server <- function(input, output, session) {
   
   # Render the filtered table
   output$table <- renderDT({
-    datatable(filtered_data(), options = list(ordering = TRUE), rownames = FALSE)
+    datatable(filtered_data(), options = list(ordering = TRUE, pageLength = 10), rownames = FALSE)
   })
   
   # Download handler
@@ -112,16 +114,16 @@ server <- function(input, output, session) {
   observeEvent(input$launchGprofiler, {
     GprofilerGeneList <- filtered_data()[["geneID"]][1:nrow(filtered_data())]
       
-    gostres <- gost(query = GprofilerGeneList, organism = "hsapiens")
+    gostres <- gost(query = GprofilerGeneList, organism = "hsapiens", evcodes = TRUE)
     
+    # Plot
     output$plot <- renderUI({
-        if (is.null(gostres) == TRUE){
+        if (is.null(gostres) == TRUE) {
           updateActionButtonStyled(session, "launchGprofiler", type="warning")
           HTML(
             as.character(div(style="color: orange", "No result to show, try with more genes / another set of genes"))
           )
-        }
-        else {
+        } else {
           gost_plot <- gostplot(gostres, capped = FALSE, interactive = TRUE)
           # Generate Gprofiler plot
           updateActionButtonStyled(session, "launchGprofiler", type="success")
@@ -130,11 +132,53 @@ server <- function(input, output, session) {
           )
         }
     })
-  })
-
-  # Volcano plot
+    
+    # Table by pathways with all genes associated
+    gostresDF <- as.data.frame(gostres$result)
+    
+    pathways_genes <- gostresDF %>% 
+      dplyr::select(term_name, source, p_value, intersection)
+    
+    # Get number of genes associated with each term/pathway element
+    gene_list <- pathways_genes$intersection
+    gene_nb <- sapply(strsplit(gene_list,","), length)
+    pathways_genes$gene_count <- gene_nb
+    
+    # Reorder dataframe
+    pathways_genes <- pathways_genes %>%
+      select(term_name, source, p_value, gene_count, intersection)
+    
+    # Print dataframe on screen
+    output$gprofilerTable <- renderDT({
+      datatable(pathways_genes, options = list(columnDefs = list(list(className = 'dt-center', targets = 3)),
+                                               order = list(2, 'asc'),
+                                               ordering = TRUE,
+                                               pageLength = 5),
+                rownames = FALSE) %>%
+        formatStyle(
+          target = 'row',
+          columns = "source",
+          backgroundColor = styleEqual(c("GO:MF", "GO:CC", "GO:BP", "KEGG", "REAC", "TF", "MIRNA", "HPA", "CORUM", "HP", "WP"),
+                                       c("#E361486D", "#46AB4B6D", "#FFAE486D", "#DD44776D", "#3366CC6D", "#5674A66D", "#23AB996D", "#6633CC6D", "#66AB016D", "#9A00996D", "#0099C66D")))
+    })
   
+  
+    # Barplot with all the terms/pathways
+    BarplotPathways <- ggplot(pathways_genes, aes(x=term_name, y=gene_count, fill=source)) +
+      geom_bar(stat="identity")
+    
+    output$plotPathways <- renderUI ({
+      renderPlot({BarplotPathways + coord_flip()})
+    })
+    
+  })
+  
+  # Volcano plot
   observeEvent(input$launchVolcano, {
+    
+    volcano_data <- select(data(), log2FoldChange,padj,gene_annot,gene_biotype)
+    volcano_data$padj <- ifelse((volcano_data$padj == 0 | -log(volcano_data$padj) > 30), exp(-30), volcano_data$padj)
+    volcano_data$gene_biotype <- ifelse(is.na(volcano_data$gene_biotype), "other", volcano_data$gene_biotype)
     
     volcano_data <- select(data(), log2FoldChange,padj,gene_annot,gene_biotype)
     volcano_data$padj <- ifelse((volcano_data$padj == 0 | -log(volcano_data$padj) > 30), exp(-30), volcano_data$padj)
