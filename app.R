3f2eea5f792ab1efdb094f11b42b0fd056d393cd
LE BARS Victor <Victor@ar155065.univ-rennes1.fr>
2024-02-07 09:38:18 +0100

    interactive gprofiler and volcano plot

--- app.R
+++ app.R
@@ -7,6 +7,8 @@ if (!require(gprofiler2)) install.packages("gprofiler2")
 if (!require(dipsaus)) install.packages("dipsaus")
 if (!require(dplyr)) install.packages("dplyr")
 if (!require(shinyjs)) install.packages("shinyjs")
+if (!require(grid)) install.packages("grid")
+if (!require(ggplotify)) install.packages("ggplotify")
 
 library(shiny)
 library(DT)
@@ -16,6 +18,8 @@ library(gprofiler2)
 library(dipsaus)
 library(dplyr)
 library(shinyjs)
+library(ggplotify)
+library(grid)
 
 # Set the size limit of uploaded files
 options(shiny.maxRequestSize = 50*1024^2)
@@ -51,7 +55,11 @@ ui <- fluidPage(
       DTOutput("table"),
       hr(),
       fluidRow(
-        column(10, plotlyOutput("plotPathways")),
+        column(9, uiOutput("plotVolcano")),
+        column(3, plotOutput("legendVolcano"))
+      ),
+      fluidRow(
+        column(10, uiOutput("plotPathways")),
         column(2, selectInput("nbTerm", tags$div("You have a lot of terms!", tags$br(),"Choose how many to display"),
                               choices = c("20","30","40","ALL"),
                               selected = "ALL",
@@ -165,9 +173,6 @@ server <- function(input, output, session) {
       pathways_genes <- pathways_genes %>%
         select(term_name, source, p_value, gene_count, intersection)
       
-      # Sort dataframe with gene count by descending order
-      pathways_genes <- pathways_genes[order(-pathways_genes$gene_count),]
-      
       # Print dataframe on screen
       output$gprofilerTable <- renderDT({
         datatable(pathways_genes, options = list(columnDefs = list(list(className = 'dt-center', targets = 3)),
@@ -184,20 +189,25 @@ server <- function(input, output, session) {
     
     
       # Barplot with all the terms/pathways
-      make_gprofiler_barplot <- function (pathways_genes_table) {
-        BarplotPathways <- ggplot(pathways_genes_table, aes(x=p_value, y=term_name, colour=source, size=gene_count)) +
+      make_gprofiler_point <- function (pathways_genes_table) {
+        BarplotPathways <- ggplot(pathways_genes_table, aes(x=p_value, y=reorder(term_name,-p_value), colour=source, size=gene_count, text=paste0("Term name: ",term_name, "<br>Source: ",source, "<br>p-value: ",p_value, "<br>Gene Count: ",gene_count))) +
           geom_point(stat="identity") +
-          guides(colour = guide_legend(override.aes = list(size = 7))) +
           scale_colour_manual(name = "Source", values = c("GO:MF"="#E36148", "GO:CC"="#46AB4B", "GO:BP"="#FFAE48", "KEGG"="#DD4477", "REAC"="#3366CC", "TF"="#5674A6",
                                        "MIRNA"="#23AB99", "HPA"="#6633CC", "CORUM"="#66AB01", "HP"="#9A0099", "WP"="#0099C6")) +
-          labs(x = "p-value", y = "", legend = "Source", title = "Enrichment terms/pathways", size = "Gene count", colour = "Source") +
+          labs(x = "p-value", y = "", legend = "Source", title = "Enrichment terms/pathways", size = "", colour = "  Source") +
           theme(legend.text = element_text(size = 9),
-                legend.title = element_text(size = 11),
-                axis.title.x = element_text(size = 11),
+                legend.title = element_text(size = 14),
+                axis.title.x = element_text(size = 14),
                 axis.text = element_text(size = 9) 
                 )
       
-        return(ggplotly(BarplotPathways + scale_x_reverse()))
+        plotlyPlot <- ggplotly(BarplotPathways + scale_x_reverse(),
+                          tooltip = "text") # Control what is display in the point label
+        
+        plotlyPlot <- plotlyPlot %>%
+          layout(legend = list(itemsizing = "constant")) # Control size of points in legend
+                 
+        return (plotlyPlot)
       }
       
      
@@ -205,31 +215,32 @@ server <- function(input, output, session) {
       if (nrow(pathways_genes) > 50) {
         shinyjs::show("nbTerm")
         observe ({
-          output$plotPathways <- renderPlotly ({
-            nbTerm_value <- as.integer(input$nbTerm)
-            if (input$nbTerm == "ALL") {
-              nbTerm_value = nrow(pathways_genes)
-            }
-            make_gprofiler_barplot(pathways_genes[1:nbTerm_value,])
+          output$plotPathways <- renderUI({ 
+            renderPlotly ({
+              nbTerm_value <- as.integer(input$nbTerm)
+              if (input$nbTerm == "ALL") {
+                nbTerm_value = nrow(pathways_genes)
+              }
+              make_gprofiler_point(pathways_genes[1:nbTerm_value,])
+            })
           })
         })
       } else {
         shinyjs::hide("nbTerm")
-        output$plotPathways <- renderPlotly ({
-          make_gprofiler_barplot(pathways_genes)
+        output$plotPathways <- renderUI({
+          renderPlotly ({
+            make_gprofiler_point(pathways_genes)
+          })
         })
       }
     }
   })
   
+  
   # Volcano plot
   observeEvent(input$launchVolcano, {
     
-    volcano_data <- select(data(), log2FoldChange,padj,gene_annot,gene_biotype)
-    volcano_data$padj <- ifelse((volcano_data$padj == 0 | -log(volcano_data$padj) > 30), exp(-30), volcano_data$padj)
-    volcano_data$gene_biotype <- ifelse(is.na(volcano_data$gene_biotype), "other", volcano_data$gene_biotype)
-    
-    volcano_data <- select(data(), log2FoldChange,padj,gene_annot,gene_biotype)
+    volcano_data <- select(data(), log2FoldChange,padj,gene_annot,gene_biotype,geneID,gene_name)
     volcano_data$padj <- ifelse((volcano_data$padj == 0 | -log(volcano_data$padj) > 30), exp(-30), volcano_data$padj)
     volcano_data$gene_biotype <- ifelse(is.na(volcano_data$gene_biotype), "other", volcano_data$gene_biotype)
     
@@ -237,23 +248,44 @@ server <- function(input, output, session) {
     thresholdPADJ <- input$thresholdSliderPADJ
     volcano_data$diff <- ifelse((volcano_data$log2FoldChange >= thresholdLOG2FC) & (volcano_data$padj <= thresholdPADJ),"UPPER", 
                                 ifelse((volcano_data$log2FoldChange <= 0-thresholdLOG2FC) & (volcano_data$padj <= thresholdPADJ), "UNDER", "NONE"))
-    volcano_data <- na.omit(volcano_data)
     
-    VolcanoPlot <- ggplot(volcano_data, aes(x = log2FoldChange, y = -log(padj), shape = gene_biotype, fill = factor(diff), size = gene_annot)) +
-    geom_point(aes(stroke = .5)) +
+    volcano_data <- filter(volcano_data, diff != "NONE")
+    volcano_data <- volcano_data[complete.cases(volcano_data$padj,volcano_data$log2FoldChange,volcano_data$gene_annot,volcano_data$gene_biotype),]
+    
+    VolcanoPlot <- ggplot(volcano_data, aes(x = log2FoldChange, y = -log(padj), shape = gene_biotype, fill = factor(diff), size = gene_annot, 
+                                            text = paste0("GeneID: ",geneID, "<br>Gene name: ",gene_name, "<br>Log2FoldChange: ",log2FoldChange, "<br>p-adjusted: ",padj))) +
+    geom_point(aes(stroke = .2)) +
+    # add some lines
+    geom_vline(xintercept = thresholdLOG2FC, linetype = "dashed", color = "grey") +
+    geom_vline(xintercept = 0-thresholdLOG2FC, linetype = "dashed", color = "grey") +
+    geom_vline(xintercept = 0, color = "black") +
     labs(x = "Log2FC", y = "-Log(p.adj)", fill="Differential Expression", size="Origin", shape="Gene Biotype") +
-    guides(fill = guide_legend(override.aes = list(size = 3, shape=21)), shape = guide_legend(override.aes = list(size = 3))) +
-    scale_fill_manual(values = c("NONE"="#9E9E9E", "UNDER"="#56B4E9", "UPPER"="#D55E00")) +
+    guides(fill = guide_legend(override.aes = list(size = 6, shape=21)), shape = guide_legend(override.aes = list(size = 6))) +
+    scale_fill_manual(values = c("UNDER"="#56B4E9", "UPPER"="#D55E00")) +
     scale_shape_manual(values = c("lncRNA" = 21 ,"protein_coding" = 23,"other" = 22)) +
-    theme(legend.text = element_text(size = 12),
-          aspect.ratio = 4/3,
-          legend.title = element_text(face = "bold", size = 14))
-  
-    VolcanoPlot <- VolcanoPlot + scale_x_continuous(limits = c(-max(abs(volcano_data$log2FoldChange)), max(abs(volcano_data$log2FoldChange))))
+    theme(legend.text = element_text(size = 16),
+          legend.title = element_text(face = "bold", size = 18))
     
-    output$plot <- renderUI({
-      renderPlot({VolcanoPlot})
+    # Extract legend to keep the ggplot type of legend
+    VolcanoLegend <- get_legend(VolcanoPlot) 
+    
+    # Declare limits of the plot & delete legend 
+    VolcanoPlot <- VolcanoPlot + scale_x_continuous(limits = c(-max(abs(volcano_data$log2FoldChange)), max(abs(volcano_data$log2FoldChange)))) +
+      theme(legend.position="none")
+    
+    # Transform into plotly object
+    VolcanoPlot <- ggplotly(VolcanoPlot,
+                            tooltip = "text")
+    
+    # Print plot
+    output$plotVolcano <- renderUI({
+      renderPlotly({VolcanoPlot})
     })
+    # Print legend
+    output$legendVolcano <- renderPlot({
+      grid.draw(VolcanoLegend)
+    })
+    
   })
 
 }
